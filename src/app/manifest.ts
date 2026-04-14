57c1c33d1030e674d368615bfffb32a401c83c1b
PrecifixDEV <suporte.precifix@gmail.com>
2026-04-14 08:40:07 -0300

    icone atualizado

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,11 +14,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/icons/android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'maskable any',
       },
       {
         src: '/icons/android-chrome-512x512.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'maskable any',
       },
     ],
   }
